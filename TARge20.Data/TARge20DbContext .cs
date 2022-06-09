d1f50d66ad0a56b4a4ce52193a33b275b70b8c5f
amaasik <75305698+amaasik@users.noreply.github.com>
2022-06-09 14:59:43 +0300

    CompanyDB

--- TARge20.Data/TARge20DbContext .cs
+++ TARge20.Data/TARge20DbContext .cs
@@ -18,5 +18,18 @@ namespace TARge20.Data
         // näide, kuidas teha, kui lisate domaini alla ühe objekti
         // migratsioonid peavad tulema siia libary-sse e TARge20.Data alla.
         public DbSet<Employee> Employee { get; set; }
-    }
+        public DbSet<AccessPermition> AccessPermitions { get; set; }
+        public DbSet<AccessLevel> AccessLevels { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<BranchOffice> BranchOffices { get; set; }
+        public DbSet<Child> Children { get; set; }
+        public DbSet<HealthInspection> HealthInspections { get; set; }
+        public DbSet<Hint> Hints { get; set; }
+        public DbSet<ItemToRent> ItemToRent { get; set; }
+        public DbSet<JobTitle> JobTitles { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<SickLeave> SickLeaves { get; set; }
+        public DbSet<Vacation> Vacations { get; set; }
+        
+     }
 }
